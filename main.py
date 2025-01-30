f6480c8b3fe2b7163c84e5788ad50dcd7d1990d8
shrika <shrikasn@gmail.com>
2025-01-30 15:30:28 +0530

    Updated_main

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from users.views import router as user_router
 from authors.views import router as author_router
 from books.views import router as books_router
-from feedback.views import router as feedback_router  # Importing feedback router
-from orders.view import router as orders_router  # Importing orders router
+from feedback.views import router as feedback_router  
+from orders.view import router as orders_router
+from api.views import router as app_router 
 
 
 app = FastAPI()
@@ -12,6 +13,8 @@ app = FastAPI()
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(author_router, prefix="/authors", tags=["Authors"])
 app.include_router(books_router, prefix="/books", tags=["Books"])
-app.include_router(feedback_router, prefix="/feedback", tags=["Feedback"])  # Adding feedback router
-app.include_router(orders_router, prefix="/orders", tags=["Orders"])  # Adding orders router
+app.include_router(feedback_router, prefix="/feedback", tags=["Feedback"]) 
+app.include_router(orders_router, prefix="/orders", tags=["Orders"])  
+
+app.include_router(app_router, prefix="/api", tags=["API"])
 
